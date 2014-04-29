d798c21fc15e33affb6ef92c702debf1ebaf2fcf
nowree <jingyuan.lao@gmail.com>
2014-04-28 22:19:21 -0700

    added a test class

---
+++ GitHub/LLtest.h
@@ -0,0 +1,13 @@
+//
+//  LLtest.h
+//  GitHub
+//
+//  Created by Shera_Lawrence on 4/28/14.
+//  Copyright (c) 2014 Shera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLtest : NSObject
+
+@end
---
+++ GitHub/LLtest.m
@@ -0,0 +1,13 @@
+//
+//  LLtest.m
+//  GitHub
+//
+//  Created by Shera_Lawrence on 4/28/14.
+//  Copyright (c) 2014 Shera. All rights reserved.
+//
+
+#import "LLtest.h"
+
+@implementation LLtest
+
+@end
